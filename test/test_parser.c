e7b06ad0efde70f10b54c439998fdbab19f64e0d
Bruce Leet <103404050+Bruceleeto@users.noreply.github.com>
2023-11-20 05:07:04 +0000

    Update test_parser.c

--- test/test_parser.c
+++ test/test_parser.c
@@ -1,166 +1,138 @@
+#include "toy_lexer.h"
 #include "toy_parser.h"
+#include "toy_compiler.h"
+#include "toy_interpreter.h"
 
 #include "toy_console_colors.h"
+
+#include "toy_memory.h"
+#include "../repl/lib_standard.h"
 
 #include "../repl/repl_tools.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <kos.h>
+
+/* KOS-specific stuff. */
+// TODO
+
+int failedAssertions = 0;
+int ignoredAssertions = 0;
+static void noAssertFn(const char* output) {
+	if (strncmp(output, "!ignore", 7) == 0) {
+		ignoredAssertions++;
+	}
+	else {
+		const char* assertionMessage = "Assertion failure: ";
+		const char* assertionResult = strstr(output, assertionMessage);
+		if (assertionResult) {
+			assertionResult += strlen(assertionMessage);
+			fprintf(stderr, TOY_CC_ERROR "%s\n", assertionResult);
+		}
+		else {
+			fprintf(stderr, TOY_CC_ERROR "Assertion failure: %s\n", output);
+		}
+		failedAssertions++;
+	}
+}
+
+
+void runBinaryCustom(const unsigned char* tb, size_t size) {
+	Toy_Interpreter interpreter;
+	Toy_initInterpreter(&interpreter);
+
+	// NOTE: Do not suppress print output
+	Toy_setInterpreterAssert(&interpreter, noAssertFn);
+    Toy_injectNativeHook(&interpreter, "standard", Toy_hookStandard);
+
+	Toy_runInterpreter(&interpreter, tb, size);
+	Toy_freeInterpreter(&interpreter);
+}
+
+void runSourceCustom(const char* source) {
+	size_t size = 0;
+	const unsigned char* tb = Toy_compileString(source, &size);
+	if (!tb) {
+		return;
+	}
+	runBinaryCustom(tb, size);
+}
+
+void runSourceFileCustom(const char* fname) {
+	size_t size = 0; // not used
+	const char* source = (const char*)Toy_readFile(fname, &size);
+	runSourceCustom(source);
+	free((void*)source);
+}
 
 int main() {
 	{
-		//source
-		const char* source = "print null;";
-
-		//test init & quit
-		Toy_Lexer lexer;
-		Toy_Parser parser;
-		Toy_initLexer(&lexer, source);
-		Toy_initParser(&parser, &lexer);
-
-		Toy_freeParser(&parser);
+		// test init & free
+		Toy_Interpreter interpreter;
+		Toy_initInterpreter(&interpreter);
+		Toy_freeInterpreter(&interpreter);
 	}
 
 	{
-		//source
+		// source
 		const char* source = "print null;";
 
-		//test parsing
+		// test basic compilation & collation
 		Toy_Lexer lexer;
 		Toy_Parser parser;
+		Toy_Compiler compiler;
+		Toy_Interpreter interpreter;
+
 		Toy_initLexer(&lexer, source);
 		Toy_initParser(&parser, &lexer);
+		Toy_initCompiler(&compiler);
+		Toy_initInterpreter(&interpreter);
 
 		Toy_ASTNode* node = Toy_scanParser(&parser);
 
-		//inspect the node
-		if (node == NULL) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode is null\n" TOY_CC_RESET);
-			return -1;
-		}
+		// write
+		Toy_writeCompiler(&compiler, node);
 
-		if (node->type != TOY_AST_NODE_UNARY || node->unary.opcode != TOY_OP_PRINT) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode is not a unary print instruction\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		if (node->unary.child->type != TOY_AST_NODE_LITERAL || !TOY_IS_NULL(node->unary.child->atomic.literal)) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode to be printed is not a null literal\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		//cleanup
-		Toy_freeASTNode(node);
-		Toy_freeParser(&parser);
-	}
-
-	{
-		//get the source file
+		// collate
 		size_t size = 0;
-		const char* source = (const char*)Toy_readFile("rd/scripts/parser_sample_code.toy", &size);
+		const unsigned char* bytecode = Toy_collateCompiler(&compiler, &size);
 
-		//test parsing a chunk of junk (valgrind will find leaks)
-		Toy_Lexer lexer;
-		Toy_Parser parser;
-		Toy_initLexer(&lexer, source);
-		Toy_initParser(&parser, &lexer);
+		Toy_runInterpreter(&interpreter, bytecode, size);
 
-		Toy_ASTNode* node = Toy_scanParser(&parser);
-
-		while (node != NULL) {
-			if (node->type == TOY_AST_NODE_ERROR) {
-				fprintf(stderr, TOY_CC_ERROR "ERROR: Error node detected\n" TOY_CC_RESET);
-				return -1;
-			}
-
-			Toy_freeASTNode(node);
-			node = Toy_scanParser(&parser);
-		}
-
-		//cleanup
+		// cleanup
+		Toy_freeASTNode(node);
 		Toy_freeParser(&parser);
-		free((void*)source);
+		Toy_freeCompiler(&compiler);
+		Toy_freeInterpreter(&interpreter);
 	}
 
 	{
-		//test parsing of escaped characters
-		const char* source = "print \"\\\"\";"; //NOTE: this string goes through two layers of escaping
+		// run each file in tests/scripts/
+		const char* filenames[] = {
+			"dreamcst.toy",
+			NULL
+		};
 
-		//test parsing
-		Toy_Lexer lexer;
-		Toy_Parser parser;
-		Toy_initLexer(&lexer, source);
-		Toy_initParser(&parser, &lexer);
+		for (int i = 0; filenames[i]; i++) {
+			printf("Running %s\n", filenames[i]);
 
-		Toy_ASTNode* node = Toy_scanParser(&parser);
+			char buffer[2048];
+			snprintf(buffer, 2048, "rd/scripts/%s", filenames[i]);
 
-		//inspect the node
-		if (node == NULL) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode is null\n" TOY_CC_RESET);
-			return -1;
+			runSourceFileCustom(buffer);
 		}
-
-		if (node->type != TOY_AST_NODE_UNARY || node->unary.opcode != TOY_OP_PRINT) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode is not a unary print instruction\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		if (node->unary.child->type != TOY_AST_NODE_LITERAL || !TOY_IS_STRING(node->unary.child->atomic.literal)) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode to be printed is not a string literal\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		if (!Toy_equalsRefStringCString(TOY_AS_STRING(node->unary.child->atomic.literal), "\"")) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode to be printed is not an escaped character, found: %s\n" TOY_CC_RESET, Toy_toCString(TOY_AS_STRING(node->unary.child->atomic.literal)));
-			return -1;
-		}
-
-		//cleanup
-		Toy_freeASTNode(node);
-		Toy_freeParser(&parser);
 	}
 
-	{
-		//test parsing of underscored numbers
-		const char* source = "print 1_000_000;";
-
-		//test parsing
-		Toy_Lexer lexer;
-		Toy_Parser parser;
-		Toy_initLexer(&lexer, source);
-		Toy_initParser(&parser, &lexer);
-
-		Toy_ASTNode* node = Toy_scanParser(&parser);
-
-		//inspect the node
-		if (node == NULL) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode is null\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		if (node->type != TOY_AST_NODE_UNARY || node->unary.opcode != TOY_OP_PRINT) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode is not a unary print instruction\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		if (node->unary.child->type != TOY_AST_NODE_LITERAL || !TOY_IS_INTEGER(node->unary.child->atomic.literal)) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode to be printed is not a string literal\n" TOY_CC_RESET);
-			return -1;
-		}
-
-		if (TOY_AS_INTEGER(node->unary.child->atomic.literal) != 1000000) {
-			fprintf(stderr, TOY_CC_ERROR "ERROR: ASTNode to be printed is not the correct value, found: %d\n" TOY_CC_RESET, TOY_AS_INTEGER(node->unary.child->atomic.literal));
-			return -1;
-		}
-
-		//cleanup
-		Toy_freeASTNode(node);
-		Toy_freeParser(&parser);
+	// 1, to allow for the assertion test
+	if (ignoredAssertions > 1) {
+		fprintf(stderr, TOY_CC_ERROR "Assertions hidden: %d\n", ignoredAssertions);
+		return -1;
 	}
 
-	printf(TOY_CC_NOTICE "All good\n" TOY_CC_RESET);
-	return 0;
+	if (failedAssertions == 0) {
+		printf(TOY_CC_NOTICE "All good\n" TOY_CC_RESET);
+	}
+
+	return failedAssertions;
 }
-
